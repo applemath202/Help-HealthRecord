246946a19a15babfdd7ffbc7578143397d541d0c
jinyan4283 <jinyan4283@gmail.com>
2019-11-18 19:57:58 +0800

    jinyinisgood

---
+++ app/src/main/java/com/jingheng/a105project/activity/DataExplan.java
@@ -0,0 +1,14 @@
+package com.jingheng.a105project.activity;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class DataExplan extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_data_explan);
+    }
+}
